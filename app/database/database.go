d3142f3fcfaafd91776eebe9cfb15a844d1ee02b
yuha <issiy0126@gmail.com>
2022-11-13 01:42:40 +0900

    User構造体を定義

--- app/database/database.go
+++ app/database/database.go
@@ -9,6 +9,13 @@ import (
 	"gorm.io/gorm"
 )
 
+type User struct {
+	Id    int    `gorm:"column:id"`
+	Uid   string `gorm:"column:user_id"`
+	UName string `gorm:"column:user_name"`
+	UPass string `gorm:"column:user_hash_password"`
+}
+
 func DBConnect() *gorm.DB {
 	info := fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("MYSQL_USER"),
