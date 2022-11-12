b2e0f19c286a9d5e806e85e42068d5af22657ccc
yuha <issiy0126@gmail.com>
2022-11-13 01:34:43 +0900

    update html出力のAPI作成

--- app/controller/handler.go
+++ app/controller/handler.go
@@ -1,1 +1,26 @@
 package controller
+
+import (
+	"Golang/session/app/database"
+	"Golang/session/app/model"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+func OutputHTML(ctx *gin.Context) {
+	session := sessions.Default(ctx)
+
+	userId := session.Get("userId").(int)
+
+	isloggedIn := model.IsLoggedIn(userId)
+
+	user := database.GetUserData()
+
+	ctx.HTML(200, "index.html", gin.H{
+		"isLoggedIn": isloggedIn,
+		"UserName":   database.user.Name,
+		"UserId":     userId,
+	})
+
+}
--- app/model/model.go
+++ app/model/model.go
@@ -1,1 +1,9 @@
 package model
+
+func IsLoggedIn(userId int) bool {
+	if userId != 0 {
+		return false
+	} else {
+		return true
+	}
+}
