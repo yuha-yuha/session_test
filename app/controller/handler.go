6587f43194fdae75813f9ec259205b02ff613ca2
yuha <issiy0126@gmail.com>
2022-11-14 13:14:40 +0900

    loginPageのハンドラ定義

--- app/controller/handler.go
+++ app/controller/handler.go
@@ -1,30 +1,9 @@
 package controller
 
 import (
-	"app/database"
-	"app/model"
-
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
-func OutputHTML(ctx *gin.Context) {
-	session := sessions.Default(ctx)
-
-	user := database.User{}
-
-	Id := session.Get("Id")
-
-	isloggedIn, Idint := model.IsLoggedIn(Id)
-
-	if isloggedIn {
-		user = database.GetUserData(Idint)
-	}
-
-	ctx.HTML(200, "index.html", gin.H{
-		"isLoggedIn": isloggedIn,
-		"UserName":   user.UName,
-		"UserId":     user.Uid,
-	})
-
+func LoginPage(ctx *gin.Context) {
+	ctx.HTML(200, "login.html", gin.H{})
 }
