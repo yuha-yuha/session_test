dd928a5a2dfeadaea07a13c45141102746644215
yuha <issiy0126@gmail.com>
2022-11-14 21:58:07 +0900

    初期ユーザ設定

--- docker/mysql/init/setting.sql
+++ docker/mysql/init/setting.sql
@@ -3,7 +3,8 @@ create table users(
     id MEDIUMINT NOT NULL AUTO_INCREMENT,
     user_id varchar(60) not null unique,
     user_name varchar(60) not null,
-    user_hash_password varchar(100) not null,
+    user_password varchar(100) not null,
     primary key(id) 
 );
 
+insert into users (user_id, user_name, user_password) value("hoge0101", "ふが", "hogehoge");
